a0a1dbcddc9fbd1c3332bc5edb2d965d5e9f0225
ansumansahu <ansumansahu2@gmail.com>
2021-12-29 22:02:40 +0530

    tuples

---
+++ Python_Basics_1/tuples.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 28 19:46:03 2021
+
+@author: Ansh
+"""
+
+#tuples : like lists but we can't modify them  
+my_tuple=(1,2,3,4,5)
+print(my_tuple[0])
+print(4 in my_tuple)
+
+new_tuple=my_tuple[1:4]
+print(new_tuple)
